a3d320b83fbe70efffe01f65fd809c60e268fb48
seo76 <rnmrnrnmrnm@gmail.com>
2023-03-15 17:16:04 +0900

    Whyrano..

--- 006 연속된 자연수의 합 구하기.py
+++ 006 연속된 자연수의 합 구하기.py
@@ -1,4 +1,4 @@
-#006연속된 자릿수..
+"""#006연속된 자릿수..
 
 import sys
 input = sys.stdin.readline
@@ -10,8 +10,8 @@ n = int(input())
 for i in range (n): #(1,n+1):
     i+=1    
     l.append(i)
-    """l2.append(int(i*(i+1)/2))
-    i+=1"""
+    l2.append(int(i*(i+1)/2))
+    i+=1
     temp +=i
     l2.append(temp)
     
@@ -23,7 +23,7 @@ m, a, b= 0, 0, 1
 
 
 while (b<n):
-    h=l2[b]-l2[a]
+    h=l[b]-l[a]
     if (h<n):
         b+=1
         print("큰:",h)
@@ -41,7 +41,70 @@ while (b<n):
 
 print(m+1)
     
+"""
 
-    
+
+"""
+
+import sys
+input = sys.stdin.readline
+l=[]
+l2=[0]
+temp=0
+n = int(input())
+
+for i in range (n):
+    i+=1    
+    l.append(i)
+    temp +=i
+    l2.append(temp)
+
+m, a, b= 0, 0, 1
+
+
+for _ in range(n):
+    h=l2[b]-l2[a]
+    if (h<n):
+        b+=1
+    if (h>n):
+        a+=1
+    if (h==n):
+        m+=1
+        a+=1
+        b+=1
+
+print(m+1)
+
+
+"""
+
+
+l=[0]
+temp, m, a, b=0, 0, 0, 1
+
+n = int(input())
+
+for i in range(n):
+    i+=1    
+    temp +=i
+    l.append(temp)
+    h=l[b]-l[a]
+    if (h<n):
+        print("큰",h,l[a],l[b],b)
+        b+=1
+        #h=l[b]-l[a]
+    elif (h>n):
+        print("작",h,l[a],l[b],b)
+        a+=1
+        #h=l[b]-l[a]
+    else:
+        print("같",h,l[a],l[b],b,"★")
+        m+=1
+        a+=1
+        b+=1
+        #h=l[b]-l[a]
+
+print(l)
+print(m+1)
 
      
