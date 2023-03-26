66f5be1054e906ba4b430fcb5debe8cdc69cb3ae
seo76 <rnmrnrnmrnm@gmail.com>
2023-03-26 23:21:15 +0900

    0082

--- 008좋은 수 구하기.py
+++ 008좋은 수 구하기.py
@@ -2,26 +2,9 @@
 n=int(input())
 Glist =list(map(int,input().split()))
 
-print(n)
-print(Glist)
+temp, a, b, m = 0, 0, 1, 0
 
-"""
-temp, a, b, m = 0, 0, 0, 0
 
-while (b<n+1):
-    if (temp == n):
-        m+=1
-        a+=1
-        b+=1
-        temp=temp+b-a
-    elif (temp < n):
-        b+=1
-        temp+=b
-    else:
-        a+=1
-        temp-=a
-print(m)
-"""
 
     
      
